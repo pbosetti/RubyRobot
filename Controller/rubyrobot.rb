12d6fd0045be5ce94e14c98b5e778382537e0279
Fabiano <fabiano@HPFabiano.(none)>
2009-09-23 18:22:27 +0200

    Now the program calculate the inverse kinematics only one time

--- Controller/rubyrobot.rb
+++ Controller/rubyrobot.rb
@@ -26,9 +26,9 @@ rescue
 end	
 puts "Connected with #{port_file}"
 
-include RubyRobot
+include InverseKinematics
 config = {
-  :l => [100.0,100.0, 10.0],
+  :l => [0.0, 100.0,100.0, 10.0],
   :home => [  0.0,  0.0,  0.0],
   :limits => [
     -90.0.to_rad..90.0.to_rad,
@@ -40,7 +40,7 @@ config = {
 r = Puma560.new(config)
 target = {:x=>100.0, :y => 100.0, :z => -10.0, :phi => -90.0.to_rad}
 r.ik(target)
-joints = r.joints[1].map {|v| v.to_deg}
+joints = r.joints.map {|v| v.to_deg}
 
 v = RobotViewer.new
 v.bodies << BoxBody.new(
@@ -74,6 +74,7 @@ server_thread = Thread.new { v.run }
 
 crosspoints = Array.new(0,Hash.new)
 crossjoints = Array.new(0,Hash.new)
+continue    = ""
 
 if ARGV[0] == "-datacapture" # MANUAL MODE
 
@@ -89,33 +90,29 @@ while running do
   line = line.map {|e| e.to_i}
   rebound = (Time.now.to_f - last_click < 1)
   begin
-    begin
-        target[:x] -= line[1]/100.0
-    	target[:y] += line[2]/100.0
-    	target[:z] += line[3]/100.0
-	    target[:phi] += line[4]/5000.0
-    	r.ik(target)
-    rescue OutOfRange
-    		# warn "Out of range!"
-        target[:x] += line[1]/100.0
-    	target[:y] -= line[2]/100.0
-    	target[:z] -= line[3]/100.0
-    	target[:phi] -= line[4]/5000.0
-    	r.ik(target)
-    end
+  	target[:x] -= line[1]/100.0
+  	target[:y] += line[2]/100.0
+   	target[:z] += line[3]/100.0
+	target[:phi] += line[4]/5000.0
+    if !r.ik(target)
+    	target[:x] += line[1]/100.0
+  		target[:y] -= line[2]/100.0
+   		target[:z] -= line[3]/100.0
+		target[:phi] -= line[4]/5000.0
+	end
 	if line[0] == 1 and ! rebound
 		if first_time_click == 0
 		    puts "------- Data capture begin --------"
     		first_time_click = Time.now.to_f
     		crosspoints[0] = {:time => 0}.merge(target)
-    		crossjoints[0] = {:time => 0, :joints => r.joints[1]}
+    		crossjoints[0] = {:time => 0, :joints => r.joints}
     	else
-    		crosspoints << {:time => Time.now.to_f-first_time_click}.merge(target)
-    		crossjoints << {:time => Time.now.to_f-first_time_click, :joints => r.joints[1]}
+    			crosspoints << {:time => Time.now.to_f-first_time_click}.merge(target)
+    		crossjoints << {:time => Time.now.to_f-first_time_click, :joints => r.joints}
     	end
     end
     v.bodies.each_with_index do |b, i|
-      		b.theta = r.joints[1][i].to_deg
+      		b.theta = r.joints[i].to_deg
     if line[0] == 1 and ! rebound
         	print b.theta, " "
     end
@@ -132,13 +129,16 @@ while running do
       last_click = Time.now.to_f
     end
   rescue
-    puts "Error: #{$!} #{line.inspect}"
+    #puts "Error: #{$!} #{line.inspect}"
   end
   sleep 0.01
 end
 #server_thread.join
+puts "Do you want to continue in automatic mode? <yes/no> :"
+continue = gets
 end
 
+if continue == "yes" || continue == "y" || continue == ""
 sp.puts "A" # AUTOMATIC MODE
 
 while !running
@@ -157,4 +157,5 @@ crossjoints.each do |cj|
     last_time = cj[:time]
 end
 server_thread.join
+end
 server_thread.exit
